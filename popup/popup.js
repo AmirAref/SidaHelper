80719ab6c6658e512b0676e1703615ab8af3a2be
Amir Aref <amiraref808@gmail.com>
2024-06-23 13:35:23 +0330

    add creator link to popup and remove content_scripts

--- popup/popup.js
+++ popup/popup.js
@@ -1,26 +1,25 @@
-async function getCurrentTabId() {
+async function getCurrentTab() {
   const queryOptions = {
     active: true,
     currentWindow: true,
   };
   const [tab] = await browser.tabs.query(queryOptions);
-  return await tab.id;
+  return await tab;
 }
 
 async function run_helper() {
-  browser.scripting.executeScript({
-    target: { tabId: await getCurrentTabId() },
-    files: ["../scripts/script.js"],
-  });
+  const sida = "https://sida.medu.ir/#/scoreClassTeacher";
+  const tab = await getCurrentTab();
+
+  // save the state
+  if (tab.url.startsWith(sida)) {
+    // excute the script
+    browser.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ["../scripts/script.js"],
+    });
+    console.log("Script Injected!");
+  }
 }
 
 document.getElementById("helper-click").addEventListener("click", run_helper);
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("link-1").addEventListener("click", () => {
-    window.open("https://my.medu.ir", "_blank");
-  });
-  document.getElementById("link-2").addEventListener("click", () => {
-    window.open("https://sida.medu.ir", "_blank");
-  });
-});
